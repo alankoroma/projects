c487a97f2eaed7ef0ea1d4eafc9595ed796af238
Inchika <inchikakoroma@hotmail.com>
2017-02-08 00:24:26 +1000

    implemented findNames

--- names/classes/BibleNames.php
+++ names/classes/BibleNames.php
@@ -19,4 +19,28 @@ class BibleNames
 
         return $bible_books;
     }
+
+    public function findNames($input)
+    {
+        $input = strtolower($input);
+        $string = preg_replace("/[^a-zA-Z]+/", "", $input);
+
+        $names = $this->getNames();
+
+        $bible_names_array = array();
+        foreach ($names as $key => $name) {
+            $book_name = strtolower($name);
+            $count = substr_count($string, $book_name);
+            $bible_names_array[$book_name] = $count;
+        }
+
+        $names_array = array();
+        foreach ($bible_names_array as $key => $name) {
+            if($name >= 1) {
+                $names_array[] = $key;
+            }
+        }
+
+        return $names_array;
+    }
 }
--- names/tests/classes/BibleNameTest.php
+++ names/tests/classes/BibleNameTest.php
@@ -15,4 +15,24 @@ class BibleNameTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('Numbers', $names);
     }
 
+    public function testFindNames()
+    {
+        $string = "it was a revelation. Some were in a jam, especially since
+                    the names of the books were not capitalized. But the truth finally
+                    struck home to numbers of our readers. To others it was a job.
+                    We want it to be a most fascinating little moment for you.
+                    Yes, there will be some really easy ones to spot.
+                    Others may require judges to help find them. I will be quickly
+                    admit it usually takes a minister to find one of them, and there
+                    will be loud lamentations when it is found. A little lady says she
+                    brews a cup of tea so she can concentrate better. See how well
+                    compete. Relax now, for there really are sixteen books of the Bible in this
+                    paragraph.  Enjoy the fun now and send back the SIXTEEN BOOKS.";
+
+        $bible_names = new BibleNames();
+        $names = $bible_names->findNames($string);
+
+        $this->assertContains('judges', $names);
+        $this->assertContains('lamentations', $names);
+    }
 }
