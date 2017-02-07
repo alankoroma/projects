83ba25b131409099d33d351010e92836cbea21d0
Inchika <inchikakoroma@hotmail.com>
2017-02-08 01:03:13 +1000

    implemented UI for find names

--- names/index.php
+++ names/index.php
@@ -3,11 +3,101 @@
 // Bootstrap
 require(dirname(__FILE__) . '/bootstrap.php');
 
-/* Load vendor and project libraries */
-require(dirname(__FILE__) . '/vendor/autoload.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Find Names</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-8">
+                    <form role="form" id="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <div class="form-group">
+                            <label for="email">Find Bible Book Names</label>
+                            <textarea class="form-control" placeholder="Enter Words" name="words" rows="5" id="comment" required></textarea>
+                        </div>
+
+                        <button type="submit" name="nameButton" class="btn btn-primary">Submit</button>
+                        <button type="reset" name="nameButton" class="btn btn-info">Reset</button>
+                    </form>
+                </div>
+                <div class="col-sm-2"></div>
+            </div>
+
+        </div>
 
 
-$bible_name = new App\BibleName();
-$bible_name->setName('Job');
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-var_dump($bible_name); exit;
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+
+<?php
+
+    if(isset($_POST['nameButton'])) {
+
+        $string = $_POST['words'];
+
+        $bibleNames = new App\BibleNames();
+        $names = $bibleNames->findNames($string);
+
+        $name_string = implode(", ", $names);
+
+        if (empty($name_string)) {
+            $name_string = 'No Bible Names Was Found';
+        }
+
+        echo "<script type='text/javascript'>
+                var $ = jQuery.noConflict();
+                $(document).ready(function(){
+                    $('#myModal').modal('show');
+                });
+            </script>";
+        }
+
+?>
+
+<!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">×</button>
+          <h4 class="modal-title">Results</h4>
+        </div>
+        <div class="modal-body">
+          <p> <?php print $name_string; ?> </p>
+        </div>
+        <div class="modal-footer">
+          <buttontype="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
